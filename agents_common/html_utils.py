bd6e13b44c18bc68ff4b92ab4e96d8404a3c0654
juga <juga@riseup.net>
2016-07-27 00:07:02 +0000

    add default encoding

--- agents_common/html_utils.py
+++ agents_common/html_utils.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+import logging
+
+logger = logging.getLogger(__name__)
 
 import html2text
-def html2md(text, encoding=None):
+def html2md(text, encoding='utf-8'):
+    logger.debug('converting to markdown')
+    logger.debug('type text %s', type(text))
     h = html2text.HTML2Text()
     h.mark_code = True
-    if isinstance(text, unicode) and encoding:
-        return h.handle(text).encode("UTF-8")
-    # FIXME: convert to unicode in case it isn't?
+    if isinstance(text, unicode):
+        return h.handle(text).encode(encoding)
+    return h.handle(text)
